7867b96544da7ef90aac3a765e1783a21833c32b
Lupita <lupitadev@gmail.com>
2019-05-30 17:33:21 -0700

    Change cover page to Spanish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edita <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
@@ -16,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
